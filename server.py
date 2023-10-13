1ef1757d9945960f12e867cc7b3af6781ab1a3b8
Roxel069 <86969414+Roxel069@users.noreply.github.com>
2023-10-13 17:56:12 +0300

    added recieve and send data to the client

--- server.py
+++ server.py
@@ -4,6 +4,8 @@ import threading
 # Define the server's IP address and port
 HOST = '127.0.0.1'
 PORT = 55555
+SIZE = 256
+FORMAT = "utf-8"
 
 # Create a socket object
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -19,15 +21,20 @@ def handle_client(client_socket, client_address):
     print(f'New connection from {client_address}')
 
     # Receive data from the client
-    while True:
-        data = client_socket.recv(256)
-        if not data:
-            break
-        print(f'Received data from {client_address}: {data.decode()}')
+    connected = True
+    while connected:
+        dataReceived = client_socket.recv(SIZE).decode(FORMAT)
+        if dataReceived == 'Quit':
+            connected = False
+        print(f'Received data from {client_address}: {dataReceived.decode()}')
+        
+        dataSent = f'Received data: {dataReceived}'
+        client_socket.send(dataSent.encode(FORMAT))
+    client_socket.close()
 
     # Close the client socket
     client_socket.close()
-    print(f'Connection from {client_address} closed')
+    print(f'Connection from {client_address} closed, msg: {data}')
 
 # Main loop to accept incoming connections and spawn new threads to handle them
 while True:
